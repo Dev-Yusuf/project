172149d0965b3ec99e79c7395f0529ac83629ed4
devyusuf <devyusuf01@gmail.com>
2024-04-25 14:38:16 +0100

    remove the dialet and partofspeech model

--- igalapedia/dictionary/admin.py
+++ igalapedia/dictionary/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from .models import Words, Dialect, PartOfSpeech
+from .models import Words
 
 # Register your models here.
 admin.site.register(Words)
-admin.site.register(Dialect)
-admin.site.register(PartOfSpeech)
-
--- igalapedia/dictionary/models.py
+++ igalapedia/dictionary/models.py
@@ -1,27 +1,13 @@
 from django.db import models
 from django.utils.text import slugify
-
-class Dialect(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
-
-class PartOfSpeech(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
  
 class Words(models.Model):
     word = models.CharField(max_length=100)
     meaning = models.CharField(max_length=200, blank=True, null=True)
     igala_meaning = models.CharField(max_length=200, blank=True, null=True)
-    part_of_speech = models.ManyToManyField(PartOfSpeech, related_name='words', blank=True)
+    part_of_speech = models.CharField(max_length=200, blank=True, null=True)
     pronunciation = models.FileField(upload_to='word_sounds/', blank=True, null=True)
-    dialects = models.ManyToManyField(Dialect, related_name='words', blank=True)
+    dialects = models.CharField(max_length=200, blank=True, null=True)
     related_terms = models.CharField(max_length=200, null=True, default=None)
     slug = models.SlugField(unique=True, max_length=100, blank=True)
 
