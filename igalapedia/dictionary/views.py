f19996f9ddb4b0c54f8b3c34b0836dc0a2b11669
devyusuf <devyusuf01@gmail.com>
2024-05-02 20:43:33 +0100

    implemented the pagination feature in the dictionary app

--- igalapedia/dictionary/views.py
+++ igalapedia/dictionary/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from .models import Words
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.utils.text import slugify
 from .filters import WordsFilters
 # Create your views here.
 
 def all_words(request):
-    words = Words.objects.all().order_by('word')[0:50]
+    words = Words.objects.all().order_by('word')
     word_filter = WordsFilters(request.GET, queryset=words)
-    words = word_filter.qs
-    context = {"words":words, "word_filter":word_filter}
+    filtered_words = word_filter.qs
+    results = 1
+    word_paginator = Paginator(filtered_words, results)
+    page_number = request.GET.get('page')
+    try:
+        current_page_holder = word_paginator.get_page(page_number)
+    except PageNotAnInteger:
+        current_page_holder = word_paginator.page(1)
+    except EmptyPage:
+        current_page_holder = word_paginator.page(word_paginator.num_pages)
+    context = {"words": current_page_holder, "word_filter": word_filter, "current_page_holder": current_page_holder}
     return render(request, "dictionary/dictionary.html", context)
 
 def singleword(request, slug):
